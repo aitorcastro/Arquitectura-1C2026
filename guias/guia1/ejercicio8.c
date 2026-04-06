d38a8744f33844d2c5e5ab74a98750b5b5235544
Aitor <132713433+aitorcastro@users.noreply.github.com>
2026-04-06 11:57:59 -0300

    ejercicio 8

---
+++ guias/guia1/ejercicio8.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+        int i = 5;
+        int j = 5;
+        printf("%d \n", i++); // devuelve i (5) y luego lo aumenta
+        printf("%d \n", ++j); // aumenta y luego devuelve
+}
