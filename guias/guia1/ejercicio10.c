4715368777efb4d2d37b84fba2428f6d8ae643e8
Aitor <132713433+aitorcastro@users.noreply.github.com>
2026-04-06 12:55:07 -0300

    ejercicio 10

---
+++ guias/guia1/ejercicio10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+        /*ciclo while*/
+        int i = 10;
+        while(i--){
+        printf("i = %d\n",i); // imprime o no el 0?
+        }
+
+        /*ahora en for*/
+        for (int i = 9; i >=0; i--){
+                printf("i = %d\n", i);
+        }
+}
