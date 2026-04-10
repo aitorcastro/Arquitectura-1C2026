d7becc518733e30573277f12ba98341c0c6ae6ec
Aitor <132713433+aitorcastro@users.noreply.github.com>
2026-04-10 11:30:28 -0300

    P2 ejercicio 7

---
+++ guias/guia2/ejercicio7.c
@@ -0,0 +1,14 @@
+/*
+        * : 
+        - int *num indica que num es un puntero a un int.
+        - *num es una desreferencia para ver el valor de lo que apunta p
+
+        & :
+        - obtiene la direccion de una variable. &x nos da la direccion de x.
+
+        void*:
+        
+Explicar qu´e suceder´ıa si la firma de la funci´on swap fuera void swap(int a, int b).
+En ese caso, ¿podr´ıamos intercambiar los valores de x e y?
+no, porque si le paso valores por referencia le paso copias y no los valores originales.
+*/
